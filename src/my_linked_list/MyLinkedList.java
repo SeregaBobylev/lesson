0620348dfb84ae35f00543db1d549a42d68569ac
Serega06122000 <bobyliov.seryozha@yandex.ru>
2022-12-08 00:07:41 +0300

    MyLinkedList version 2

--- src/my_linked_list/MyLinkedList.java
+++ src/my_linked_list/MyLinkedList.java
@@ -2,30 +2,62 @@ package my_linked_list;
 
 public class MyLinkedList<E> {
     public int size = 0;
-    private Node<E> node;
-    private static class Node<E> {
-        private E value;
-        public Node next;
-        private Node(E value, Node<E> next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
+    private Node<E> nodeList;
 
     public MyLinkedList() {
     }
+
     public void add(E value) {
-        if(node==null){
-            node = new Node<>(value,null);
-        }
-        else {
-           Node<E> tempNode = node;
-           node = new Node<>(value,tempNode);
-        }
+        nodeList = nodeList == null ? new Node<>(value, null) : new Node<>(value, nodeList);
         size++;
     }
 
+    public boolean contains(E value) {
+        Node<E> tempList = nodeList;
+        while (tempList != null){
+            if (tempList.value == value) return true;
+            else tempList = tempList.nextNode;
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        Node<E> tempList = nodeList;
+        int tempPosition = size;
+        if (index >= size || index < 0) return null;
+        while (tempList != null) {
+            tempPosition--;
+            if (index == tempPosition) return tempList.value;
+            tempList = tempList.nextNode;
+        }
+        return null;
+    }
+
     public boolean remove(E value) {
-        return true;
+        Node<E> lastList = null, tempList = nodeList;
+
+        while (tempList != null){
+            if (tempList.value == value) {
+                lastList.nextNode = tempList.nextNode;
+                nodeList = lastList;
+                size--;
+                return true;
+            }
+            lastList = tempList;
+            tempList = tempList.nextNode;
+        }
+        return false;
+    }
+    public boolean remove(int index){
+        return false;
+    }
+    private static class Node<E> {
+        private E value;
+        public Node nextNode;
+
+        private Node(E value, Node<E> next) {
+            this.value = value;
+            this.nextNode = next;
+        }
     }
 }
--- src/my_linked_list/Test.java
+++ src/my_linked_list/Test.java
@@ -3,12 +3,19 @@ package my_linked_list;
 import java.util.LinkedList;
 
 public class Test {
-    public static void main(String[] args) {
-        MyLinkedList<String> listEmail = new MyLinkedList<>();
+    static MyLinkedList<String> listEmail = new MyLinkedList<>();
+    static {
         listEmail.add("serega@mail.com");
         listEmail.add("olga@mail.com");
         listEmail.add("oleg@mail.com");
         listEmail.add("alex@mail.com");
-        System.out.println(listEmail.size);
+        listEmail.add("andrey@mail.com");
     }
+    public static void main(String[] args) {
+        System.out.println(listEmail.remove("oleg@mail.com"));
+        System.out.println(listEmail.contains("serega@mail.com"));
+        System.out.println(listEmail.contains("oleg@mail.com"));
+        System.out.println(listEmail.get(1));
+    }
+
 }
